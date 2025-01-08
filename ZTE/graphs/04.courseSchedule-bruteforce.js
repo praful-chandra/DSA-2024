a15ee55bcd74418657ebcb0af6d8003f9992aa32
Praful Chandra <chandra.s.praful@gmail.com>
2025-01-08 16:54:51 +0530

    207. Course Schedule - Bruteforce

---
+++ ZTE/graphs/04.courseSchedule-bruteforce.js
@@ -0,0 +1,44 @@
+function canFinish(numCourses, prerequisites) {
+  const adjList = Array(numCourses).fill([]);
+
+  prerequisites.forEach((cPair) => {
+    adjList[cPair[1]] = [...adjList[cPair[1]], cPair[0]];
+  });
+  let result = true;
+  for (let thisCourse = 0; thisCourse < numCourses; thisCourse++) {
+    if (checkIfCycleExist(thisCourse, adjList)) {
+      result = false;
+      break;
+    }
+  }
+  return result;
+}
+
+function checkIfCycleExist(thisCourse, adjList) {
+  const queue = [];
+  queue.push(...adjList[thisCourse]);
+  const seenCourses = {
+    thisCourse: true,
+  };
+  let result = false;
+  while (queue.length) {
+    const currCourse = queue.shift();
+    if (currCourse === thisCourse) {
+      result = true;
+      break;
+    }
+    seenCourses[currCourse] = true;
+    queue.push(...adjList[currCourse].filter((c) => !seenCourses[c]));
+  }
+  return result;
+}
+
+console.log(
+  canFinish(4, [
+    [2, 0],
+    [1, 0],
+    [3, 1],
+    [3, 2],
+    [1, 3],
+  ])
+);
